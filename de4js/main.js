07d7fa82ca8dbfa44927d9a05f3d9860f4e79452
teachwiki <35622257+teachwiki@users.noreply.github.com>
2020-12-31 08:00:24 -0800

    Update main.js

--- de4js/main.js
+++ de4js/main.js
@@ -173,7 +173,7 @@
       if (temp === '') return;
 
       if (!workerFormat) {
-        workerFormat = new Worker('https://rawcdn.githack.com/teachwiki/tools/5b47053e9c95f54ca6927d6e9f279ec9ee0fc694/de4js/worker/format.js');
+        workerFormat = new Worker('/de4js/worker/format.js');
         workerFormat.addEventListener('message', function (e) {
           if (!e.data.highlight) {
             downloadResult(e.data.result);
@@ -243,7 +243,7 @@
       }
 
       if (!workerDecode) {
-        workerDecode = new Worker('https://rawcdn.githack.com/teachwiki/tools/5b47053e9c95f54ca6927d6e9f279ec9ee0fc694/de4js/worker/decode.js');
+        workerDecode = new Worker('/de4js/worker/decode.js');
         workerDecode.addEventListener('message', function (e) {
           if (e.data !== temp) {
             temp = e.data;
